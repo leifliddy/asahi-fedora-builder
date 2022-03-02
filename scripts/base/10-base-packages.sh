7a8bb6b254c2bc370e5f03359985cdafa194b80b
Hector Martin <marcan@marcan.st>
2022-03-03 03:10:05 +0900

    10-base-packages.sh: Add iwd for WiFi support
    Closes: #1
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- scripts/base/10-base-packages.sh
+++ scripts/base/10-base-packages.sh
@@ -3,4 +3,4 @@ set -e
 
 pacman --noconfirm -R linux-aarch64
 pacman --noconfirm -Syu
-pacman --noconfirm -S asahi-scripts mkinitcpio linux-asahi grub
+pacman --noconfirm -S asahi-scripts mkinitcpio linux-asahi grub iwd
