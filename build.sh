cbbf118b8c6c23fb0239253676624c7247b5ca8f
Leif Liddy <leifliddy@users.noreply.github.com>
2023-12-18 07:53:55 +0100

    modified sudo check

--- build.sh
+++ build.sh
@@ -16,7 +16,10 @@ EFI_UUID=2ABF-9F91
 BOOT_UUID=$(uuidgen)
 BTRFS_UUID=$(uuidgen)
 
-(([[ $USER != 'root' ]] || [[ $SUDO_USER != 'root' ]])) && echo "You must run this script as root and not with sudo" && exit 1
+if [ $USER != 'root' ] || [ $SUDO_USER != 'root' ]; then
+    echo "You must run this script as root and not with sudo"
+    exit 1
+fi
 
 [ ! -d $mnt_image ] && mkdir $mnt_image
 [ ! -d $mkosi_output ] && mkdir $mkosi_output
